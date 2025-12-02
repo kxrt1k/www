cc4141641a2f2518d4bfcdac7c8724841eb68880
MAZE <remvze@proton.me>
2025-12-02 20:01:21 +0330

    feat: add animated placeholder

--- src/data/experiments.ts
+++ src/data/experiments.ts
@@ -9,4 +9,9 @@ export const experiments = [
     url: "/experiments/animated-number-counter",
     date: "2025-12-02",
   },
+  {
+    title: "Animated Placeholder",
+    url: "/experiments/animated-placeholder",
+    date: "2025-12-02",
+  },
 ];
