085ef4a9768970d96ad95ddf5b0064691b542ba1
MAZE <remvze@proton.me>
2025-12-03 19:57:03 +0330

    chore: add projects

---
+++ src/data/projects.ts
@@ -0,0 +1,37 @@
+export const projects = [
+  {
+    title: "OPN",
+    description: "Your open-source bio page.",
+    url: "https://opn.bio",
+  },
+  {
+    title: "Haus",
+    description: "Dead-simple online workstation.",
+    url: "https://use.haus",
+  },
+  {
+    title: "GitViews",
+    description: "Simple GitHub view counter.",
+    url: "https://gitviews.com",
+  },
+  {
+    title: "D.E.",
+    description: "Curated list of Design Engineers.",
+    url: "https://designengineers.net",
+  },
+  {
+    title: "Nothing",
+    description: "Simply do nothing and just be.",
+    url: "https://designengineers.net",
+  },
+  {
+    title: "Nicefolios",
+    description: "Archive of exceptional portfolios.",
+    url: "https://nicefolios.com",
+  },
+  {
+    title: "Minimum",
+    description: "Minimal websites directory.",
+    url: "https://mnmm.xyz",
+  },
+];
